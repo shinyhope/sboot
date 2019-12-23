3ef3133b95bf32cd4902325557da497142261d0c
shinyhope <shinyhope@github.com>
2019-12-23 11:14:44 +0900

    회원가입, 사용자목록 구현, model, list, redirect 사용

--- src/main/java/net/sboot/web/UserController.java
+++ src/main/java/net/sboot/web/UserController.java
@@ -1,21 +1,34 @@
 package net.sboot.web;
 
+import java.util.ArrayList;
+import java.util.List;
+
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 
 @Controller
 public class UserController {
 
-	@GetMapping
-	public String create() {
-		
+	private List<User> users = new ArrayList<User>();
+	
+//	@GetMapping("/create")	
+//	public String create(String userId, String password, String name, String email ) {
+	@PostMapping("/create")
+//	public String create(String userId, String password, String name, String email ) {
+	public String create(User user) {	
+	
+//		System.out.println("userId:" + userId + ", name:" + name + ", email: " + email);
+		System.out.println("user: " + user);
+		users.add(user);
+//		return "index";
 		return "redirect:/list";
 	}
 
-	@PostMapping
-	public String list() {
-		
+	@GetMapping("/list")
+	public String list(Model model) {
+		model.addAttribute("users", users);
 		return "list";
 
 	}
--- src/main/java/net/sboot/web/WelcomeController.java
+++ src/main/java/net/sboot/web/WelcomeController.java
@@ -1,5 +1,26 @@
 package net.sboot.web;
 
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+
+@Controller
 public class WelcomeController {
+
+	/*
+	 * 메서드 이름은 중요하지 않다. 
+	 * return 옆에 문자열이 중요하다.
+	 * getmapping : url 지정 역할
+	 * return 옆은 template 이름 
+	 * 
+	 */
+	@GetMapping("/helloworld")
+	public String welcome(String name, int age, Model model) {
+		System.out.println("name:" + name);
+		model.addAttribute("name",  name);
+		model.addAttribute("age",  age);
+		return "welcome";
+
+	}
 
 }
